9837e46600dfd24ff8e665786717adfa1140e7fc
Mustaec R.F <scorpio-84@mail.ru>
2022-08-05 18:04:47 +0300

    lesson_1 Corect login User, add hash password

--- TODO/authapp/management/commands/users.py
+++ TODO/authapp/management/commands/users.py
@@ -1,12 +1,17 @@
 from django.core.management.base import BaseCommand
 from authapp.models import User
+from django.contrib.auth.hashers import make_password
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         User.objects.all().delete()
-        User.objects.create_superuser(username='MadEngine1', email='1@vk.ru', password='1')
-        User.objects.create(username='Test_user_1', email='test_user_1@vk.ru', password='12')
-        User.objects.create(username='Test_user_2', email='test_user_2@vk.ru', password='13')
-        User.objects.create(username='Test_user_3', email='test_user_3@vk.ru', password='14')
+        User.objects.create_superuser(username='MadEngine1', first_name='Руслан', last_name='Мустаев', email='1@vk.ru',
+                                      password='1')
+        User.objects.create(username='Test_user_1', first_name='Тест_1', last_name='Тест_1', email='test_user_1@vk.ru',
+                            password=make_password('12'))
+        User.objects.create(username='Test_user_2', first_name='Тест_2', last_name='Тест_2', email='test_user_2@vk.ru',
+                            password=make_password('13'))
+        User.objects.create(username='Test_user_3',  first_name='Тест_2', last_name='Тест_2', email='test_user_3@vk.ru',
+                            password=make_password('14'))
 
