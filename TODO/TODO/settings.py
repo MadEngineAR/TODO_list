54572e5564d8ff10a090997a5535583ec3c5eec2
Mustaec R.F <scorpio-84@mail.ru>
2022-08-23 21:07:32 +0500

    lesson_6_add_jwt_auth_requests(No to Postman)

--- TODO/TODO/settings.py
+++ TODO/TODO/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-
     # TO_DO
     'rest_framework',
     'django_filters',
@@ -45,7 +44,6 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
-
 
 ]
 
@@ -156,16 +154,15 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated',],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated', ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-    # 'rest_framework.authentication.BasicAuthentication',
-    # 'rest_framework.authentication.TokenAuthentication',
-    'rest_framework_simplejwt.authentication.JWTAuthentication',
-    'rest_framework.authentication.SessionAuthentication'
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
     ]
 
-
-    }
+}
 
 # JSON_CAMEL_CASE = {
 #     'RENDERER_CLASS': 'rest_framework.renderers.UnicodeJSONRenderer'
