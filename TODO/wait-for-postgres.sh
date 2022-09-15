a4fc8eb5ec17a879b6af2b837c7cd5fb83fa9f69
Mustaec R.F <scorpio-84@mail.ru>
2022-09-15 19:10:41 +0500

    lesson_12_add_backend_gunicorn

--- TODO/wait-for-postgres.sh
+++ TODO/wait-for-postgres.sh
@@ -4,8 +4,11 @@ set -e
 host="$1"
 shift
 cmd="$@"
-until PGPASSWORD="1" psql -h "$host" -d "db" -U "Rus" -c '\q';>
+
+until PGPASSWORD="1" psql -h "$host" -d "db" -U "Rus" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
->&2 echo "Postgres is up - executing command" exec $cmd
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
